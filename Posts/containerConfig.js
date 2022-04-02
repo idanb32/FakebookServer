49f493a52ad40b44556c46a7ed9c70b60e8a9fed
idanb32 <bsl3232@gmail.com>
2022-04-02 12:39:09 +0300

    removedPost

--- Posts/containerConfig.js
+++
@@ -1,9 +0,0 @@
-const awilix = require("awilix");
-const config = require("config");
-
-const container = awilix.createContainer( {injectionMode: awilix.InjectionMode.CLASSIC});
-
-container.register({
-    config: awilix.asValue(config)
-});
-module.exports = container;
